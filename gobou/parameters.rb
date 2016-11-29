fdf009c5066b6e19fdb70c0f3aca795cec1f9c24
Takayuki Ujiie <takau@easter.kuee.kyoto-u.ac.jp>
2016-11-29 14:43:54 +0900

    initial commit

---
+++ gobou/parameters.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+# ERB(Meta) parameter
+$debug = true
+$project    = `pwd`.chomp.split('/rtl')[0]
+$d_mac      = 3   # Delay clocks in the pool module
+$d_bias     = 2   # Delay clocks in the pool module
+$d_relu     = 2   # Delay clocks in the pool module
+
+### Verilog parameter
+# Hardware
+$step     = 10
+$core     = 32
+$core_log = (Math.log($core)/Math.log(2)).to_i
+$dwidth   = 16
+$lwidth   = 12
+$imgsize  = 11 # Image size: 800 + 500 + 10
+$netsize  = 14 # Network size: (801 * 500 + 501 * 10) / 32
+
+# Network
+$n_in = 800
+$n_f1 = 500
+$n_f2 = 10
---
+++ renkon/parameters.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+### ERB(Meta) parameter
+$debug      = false
+$project    = `pwd`.chomp.split('/rtl')[0]
+$core       = 8   # Num of cores
+$core_log   = (Math.log($core)/Math.log(2)).to_i
+$tech       = nil # TODO: introduce mapping of specific devices.
+$max_size   = 32  # Corresponds to acceptable max input size
+$max_line   = 5
+$d_pixelbuf = 32  # Corresponds to acceptable max input size
+$d_poolbuf  = 32  # Corresponds to acceptable max input size
+$d_conv     = 5   # Delay clocks in the conv module
+$d_accum    = 1   # Delay clocks in the accumlator module
+$d_bias     = 2   # Delay clocks in the pool module
+$d_relu     = 2   # Delay clocks in the pool module
+$d_pool     = 2   # Delay clocks in the pool module
+
+### Verilog parameter
+# Hardware
+$dwidth   = 16 # Data bitwidth
+$lwidth   = 8  # Literal(Constant) data bitwidth
+$step     = 10 # Step width(ns) per clock
+$imgsize  = 13 # Address bitwidth of inputs: (12*12*20)
+$outsize  = 10 # Address bitwidth of outputs: (12*12*20)
+$netsize  = 12 # Address bitwidth of weights: (50*20*5*5)/8
+$faccum   = 10 # Address bitwidth of conv fmap: (24*24)
+$bufsize  = ( Math.log($max_size) / Math.log(2) ).to_i;;
+
+# Network
+$n_in   = 1
+$n_f1   = 20
+$n_f2   = 50
+$fsize  = 5
+$psize  = 2
+$in_hei = 28
+$fm1hei = $in_hei - $fsize + 1
+$pm1hei = $fm1hei / $psize
+$fm2hei = $pm1hei - $fsize + 1
+$pm2hei = $fm2hei / $psize
+
