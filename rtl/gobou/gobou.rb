1bc89eb1b5ee20f46e41945941c09c23920d5723
Takayuki Ujiie <takau@easter.kuee.kyoto-u.ac.jp>
2017-04-24 19:47:07 +0900

    many revisions...

--- rtl/gobou/gobou.rb
+++ rtl/gobou/gobou.rb
@@ -1,23 +1,21 @@
 #!/usr/bin/env ruby
 
+require '../ninjin/ninjin'
+
 # ERB(Meta) parameter
-$dist     = false
-$project  = `pwd`.chomp.split('/rtl')[0]
+$core     = $gobou_core
+$core_log = $gobou_core_log
+
 $d_mac    = 3   # Delay clocks in the pool module
 $d_bias   = 2   # Delay clocks in the pool module
 $d_relu   = 2   # Delay clocks in the pool module
 
 ### Verilog parameter
 # Hardware
-$step     = 10
-$core     = 16
-$core_log = (Math.log($core)/Math.log(2)).to_i
-$dwidth   = 16
-$lwidth   = 10
-$imgsize  = 12 # Image size: 800 + 500 + 10
-$netsize  = 14 # Network size: (801 * 500 + 501 * 10) / 32
+$netsize  = $gobou_netsize # Network size: (801 * 500 + 501 * 10) / $core
 
 # Network
-$n_in = 512
-$n_f1 = 256
+$n_in = 800
+$n_f1 = 500
 $n_f2 = 10
+
