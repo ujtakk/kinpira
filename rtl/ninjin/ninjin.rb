d9f7e3dea2fc78d770dc0560bc553ffccc219437
Takayuki Ujiie <takau@easter.kuee.kyoto-u.ac.jp>
2017-06-06 17:17:18 +0900

    mod

--- rtl/ninjin/ninjin.rb
+++ rtl/ninjin/ninjin.rb
@@ -3,12 +3,12 @@ $step             = 10
 $port             = 32
 $dwidth           = 16
 $lwidth           = 10
-$imgsize          = 10
+$imgsize          = 16
 
 $renkon_core      = 4
-$renkon_netsize   = 10
+$renkon_netsize   = 16
 $gobou_core       = 4
-$gobou_netsize    = 10
+$gobou_netsize    = 16
 
 $renkon_core_log  = (Math.log($renkon_core)/Math.log(2)).to_i
 $gobou_core_log   = (Math.log($gobou_core)/Math.log(2)).to_i
