8a7cd8a5eb8dae782e59d98a984512546727c1b1
Takayuki Ujiie <takau@easter.kuee.kyoto-u.ac.jp>
2017-06-06 17:45:16 +0900

    rtl/gobou: gobou test retriebe

--- rtl/ninjin/ninjin.rb
+++ rtl/ninjin/ninjin.rb
@@ -5,9 +5,9 @@ $dwidth           = 16
 $lwidth           = 10
 $imgsize          = 16
 
-$renkon_core      = 4
+$renkon_core      = 8
 $renkon_netsize   = 16
-$gobou_core       = 4
+$gobou_core       = 16
 $gobou_netsize    = 16
 
 $renkon_core_log  = (Math.log($renkon_core)/Math.log(2)).to_i
